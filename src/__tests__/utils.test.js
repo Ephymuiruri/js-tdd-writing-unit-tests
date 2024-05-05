2a10bbafc89a913f898e57490f714343e83924e9
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-05-05 08:39:17 +0300

    handles words both in upper and lower case

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -9,4 +9,9 @@ describe("pointsForWord",()=>{
         //test
         expect(points).toBe(7)
     })
-})
+},
+it("handles uppercase and lowercase input",()=>{
+    const word ="tEsT"
+    const points =pointsForWord(word);
+    expect(points).toBe(7)
+}))
