549dc6463ee6cd77a82b63c24167dbb733de83ec
Ephy Muiruri Wachira <ephywachira89@gmail.com>
2024-05-05 08:24:16 +0300

    test passing

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -1,1 +1,12 @@
 // Your tests here
+import {pointsForWord} from "../utils"
+describe("pointsForWord",()=>{
+    it("calculates the total points for word(1 point per vowel, 2points per consonant)",()=>{
+        //Arrange
+        const word ="test";
+        //Act
+        const points = pointsForWord(word)
+        //test
+        expect(points).toBe(7)
+    })
+})
